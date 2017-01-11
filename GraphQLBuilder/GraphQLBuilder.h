fcd3c205e080373f3eaaae1ef611e16574d73e4b
Wipoo Shinsirikul <mylifeasdog@gmail.com>
2017-01-11 18:32:36 +0700

    Initial Commit

---
+++ GraphQLBuilder/GraphQLBuilder.h
@@ -0,0 +1,19 @@
+//
+//  GraphQLBuilder.h
+//  GraphQLBuilder
+//
+//  Created by Wipoo Shinsirikul on 1/11/17.
+//  Copyright © 2017 Wipoo Shinsirikul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GraphQLBuilder.
+FOUNDATION_EXPORT double GraphQLBuilderVersionNumber;
+
+//! Project version string for GraphQLBuilder.
+FOUNDATION_EXPORT const unsigned char GraphQLBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraphQLBuilder/PublicHeader.h>
+
+
